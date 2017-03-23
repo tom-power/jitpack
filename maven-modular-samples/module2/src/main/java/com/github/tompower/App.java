135d6a412f399537890b679d4b2495bfe1a6a6fb
Tom Power <tpower21@gmail.com>
2017-03-23 08:46:02 +0000

    first commit

---
+++ maven-modular-samples/module2/src/main/java/com/github/tompower/App.java
@@ -0,0 +1,15 @@
+package com.github.tompower;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String GREETING = "Hello World!";
+    
+    public static void main( String[] args )
+    {
+        System.out.println( GREETING );
+    }
+}
